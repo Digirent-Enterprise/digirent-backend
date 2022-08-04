69a121774538671627dd8addb3bec3c2ab17e76a
s3822042 <s3822042@rmit.edu.vn>
2022-08-04 17:40:44 +0700

    fix: merge error

--- models/user.model.js
+++ models/user.model.js
@@ -2,57 +2,9 @@ const mongoose = require("mongoose");
 const validator = require("validator");
 
 const userSchema = mongoose.Schema(
-    {
-        name: {
-            type: String,
-        },
-        email: {
-            type: String,
-            required: true,
-            unique: true,
-            trim: true,
-            lowercase: true
-        },
-
-        password: {
-            type: String,
-            required: true,
-            trim: true,
-            minlength: 8,
-            validate(value) {
-                console.log('valiueeee', value)
-                if (!value.match(/\d/) || !value.match(/[a-zA-Z]/)) {
-                    throw new Error('Password must contain at least one letter and one number');
-                }
-            },
-            private: true, // used by the toJSON plugin
-            selected: false
-        },
-
-        role: {
-            type: String,
-            default: 'user',
-        },
-
-        isEmailVerified: {
-            type: Boolean,
-            default: false,
-        },
-
-        token: {
-            type: String,
-            selected: false
-        },
-
-        phone: {
-            type: String,
-            trim: true,
-            minlength: 9
-        },
-
-        createdDate: {
-            type: String
-        }
+  {
+    name: {
+      type: String,
     },
     email: {
       type: String,
@@ -76,6 +28,7 @@ const userSchema = mongoose.Schema(
         }
       },
       private: true, // used by the toJSON plugin
+      selected: false,
     },
 
     role: {
@@ -90,6 +43,7 @@ const userSchema = mongoose.Schema(
 
     token: {
       type: String,
+      selected: false,
     },
 
     phone: {
@@ -97,14 +51,12 @@ const userSchema = mongoose.Schema(
       trim: true,
       minlength: 9,
     },
-    picture: {
-      type: String,
-    },
 
     createdDate: {
       type: String,
     },
   },
+
   { typeKey: "type" },
 );
 
