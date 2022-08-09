65ba5f81a8b6ed18dd488570021605e45f875f5e
s3822042 <s3822042@rmit.edu.vn>
2022-08-09 18:39:25 +0700

    fix: typo

--- routes/v1/transaction.route.js
+++ routes/v1/transaction.route.js
@@ -1,14 +1,29 @@
-const transactionController = require('../../controllers/transaction.controller');
+const transactionController = require("../../controllers/transaction.controller");
 
-const {authenticateAdmin, authenticateToken} = require('../../middlewares/auth')
+const {
+  authenticateAdmin,
+  authenticateToken,
+} = require("../../middlewares/auth");
 
-const router = require('express').Router();
+const router = require("express").Router();
 
-router.get('/', authenticateAdmin ,(req,res) => transactionController.getALlTransaction(req,res));
-router.get('/transaction-detail', authenticateToken, (req, res) => transactionController.getTransactionDetail(req,res));
-router.delete('/delete-transaction', authenticateAdmin, (req, res) => transactionController.deleteTransaction(req,res));
-router.put('/update-transaction', authenticateAdmin, (req, res) => transactionController.changeTransactionStatus(req,res));
-router.get('/user-transaction', authenticateToken, (req,res)=> transactionController.getTransactionByUserEmail(req, res));
-router.post('/create-transaction', authenticateAdmin, (req,res)=> transactionController.createTransaction(req, res));
+router.get("/", authenticateAdmin, (req, res) =>
+  transactionController.getAllTransaction(req, res),
+);
+router.get("/transaction-detail", authenticateToken, (req, res) =>
+  transactionController.getTransactionDetail(req, res),
+);
+router.delete("/delete-transaction", authenticateAdmin, (req, res) =>
+  transactionController.deleteTransaction(req, res),
+);
+router.put("/update-transaction", authenticateAdmin, (req, res) =>
+  transactionController.changeTransactionStatus(req, res),
+);
+router.get("/user-transaction", authenticateToken, (req, res) =>
+  transactionController.getTransactionByUserEmail(req, res),
+);
+router.post("/create-transaction", authenticateAdmin, (req, res) =>
+  transactionController.createTransaction(req, res),
+);
 
 module.exports = router;
