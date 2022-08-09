0f521e4c495835a5938dfa06d7036fc6e18b9743
OPSWAT\bao.vo <bao.vo@opswat.com>
2022-08-09 17:54:40 +0700

    feat: add upload image to Cloudinary

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,10 +1,12 @@
 const {Product, Category} = require("../models");
+const {uploadMultipleFiles} = require("../services/cloudinary.service");
+const {CloudinaryService} = require("../services");
 
 const productController = {
     //add product
-    addProduct: async(req, res) => {
+    addProduct: async (req, res) => {
         const files = req.files;
-        try{
+        try {
             // const newProduct = new Product(req.body);
             const newProduct = new Product({
                 name: req.body.name,
@@ -17,67 +19,69 @@ const productController = {
                 status: req.body.status,
                 serial: req.body.serial,
             });
-            if(files){
-                req.files.forEach(function(file, index, arr){
-                    let path = file.path + ','
-                    path = path.substring(0, path.lastIndexOf(","))
-                    newProduct.images.push(path)
-                })
+            if (files) {
+                newProduct.images = await uploadMultipleFiles(files);
             }
-
             const savedProduct = await newProduct.save();
-            if(req.body.category){
+            if (req.body.category) {
                 // const category = Category.findById(req.body.category);
                 const category = Category.findById(req.body.category);
                 await category.updateOne({$push: {products: savedProduct._id}});
             }
             res.status(200).json(savedProduct);
 
-        } catch(err){
+        } catch (err) {
             res.status(500).json(err);
         }
     },
 
     // get all products
-    getAllProducts: async(req, res) => {
+    getAllProducts: async (req, res) => {
         try {
             const products = await Product.find();
             res.status(200).json(products);
-        } catch(err) {
+        } catch (err) {
             res.status(500).json(err);
         }
     },
 
     //get product
-    getProduct: async(req, res) => {
+    getProduct: async (req, res) => {
         try {
             const product = await Product.findById(req.params.id).populate('category');
             res.status(200).json(product);
-        } catch(err) {
+        } catch (err) {
             res.status(500).json(err);
         }
     },
 
     // update product
-    updateProduct: async(req, res) => {
-        try{
+    updateProduct: async (req, res) => {
+        try {
             const product = await Product.findById(req.params.id);
             await product.updateOne({$set: req.body});
             res.status(200).json('Update successfully!');
-        } catch(err){
+        } catch (err) {
             res.status(500).json(err);
         }
     },
 
     // delete product
-    deleteProduct: async(req, res) => {
-        try{
+    deleteProduct: async (req, res) => {
+        try {
             await Category.updateMany({products: req.params.id}, {$pull: {products: req.params.id}});
             await Product.findByIdAndDelete(req.params.id);
             res.status(200).json('Delete successfully!');
-        } catch(err){
+        } catch (err) {
             res.status(500).json(err);
         }
+    },
+
+    uploadSingleImage: async (req, res) => {
+        const file = req.files[0];
+        const urlObj = await CloudinaryService.uploadSingleFile(file);
+        if (!urlObj) res.sendStatus(404);
+        return res.json({url: urlObj.url});
     }
 
 };
--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,17 +1,3 @@
 const multer = require ('multer');
-const path = require('path');
-
-const storage = multer.diskStorage({
-    destination: function(req, files, cb){
-        cb(null, path.join(__dirname, '../upload/'));
-    },
-    filename: function(req, files, cb){
-        cb(null, files.originalname);
-    }
-});
-
-const upload =  multer({
-        storage: storage
-    }).array('images', 10);
-
+const upload =  multer().array('images', 10);
 module.exports = upload;
