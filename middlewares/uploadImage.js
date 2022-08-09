c800482418e25dfd1fa942572f2cee595e84d099
s3822042 <s3822042@rmit.edu.vn>
2022-08-09 11:10:08 +0700

    feat: not fix bugs yet for cloudinary

--- middlewares/uploadImage.js
+++ middlewares/uploadImage.js
@@ -1,17 +1,27 @@
-const multer = require ('multer');
-const path = require('path');
+const multer = require("multer");
+const path = require("path");
 
 const storage = multer.diskStorage({
-    destination: function(req, files, cb){
-        cb(null, path.join(__dirname, '../upload/'));
-    },
-    filename: function(req, files, cb){
-        cb(null, files.originalname);
-    }
+  destination: function (req, files, cb) {
+    cb(null, path.join(__dirname, "../upload/"));
+  },
+  filename: function (req, files, cb) {
+    cb(null, files.originalname);
+  },
 });
 
-const upload =  multer({
-        storage: storage
-    }).array('images', 10);
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype === "image/jpeg" || file.mimetype === "image/png") {
+    cb(null, true);
+  } else {
+    //reject file
+    cb({ message: "Unsupported file format" }, false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+}).array("images", 10);
 
 module.exports = upload;
--- services/index.js
+++ services/index.js
@@ -1,5 +1,6 @@
-module.exports.UserService = require('./user.service');
-module.exports.TokenService = require('./token.service');
-module.exports.AuthService = require('./auth.service');
-module.exports.EmailService = require('./email.service');
-module.exports.TransactionService = require('./transaction.service')
+module.exports.UserService = require("./user.service");
+module.exports.TokenService = require("./token.service");
+module.exports.AuthService = require("./auth.service");
+module.exports.EmailService = require("./email.service");
+module.exports.TransactionService = require("./transaction.service");
+module.exports.CloudinaryService = require("./cloudinary.service");
