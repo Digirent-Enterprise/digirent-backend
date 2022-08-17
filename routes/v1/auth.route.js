b960546b0ac6e7470e903c59302f191458f1aba3
bee <vogiabao292@gmail.com>
2022-08-17 19:45:57 +0700

    feat: add changes

--- routes/v1/auth.route.js
+++ routes/v1/auth.route.js
@@ -7,7 +7,7 @@ const {
 const router = express.Router();
 router.post("/register", (req, res) => authController.register(req, res));
 router.post("/login", (req, res) => authController.login(req, res));
-router.post("/refresh-tokens", authenticateToken, (req, res) =>
+router.post("/refresh-tokens", (req, res) =>
   authController.refreshTokens(req, res),
 );
 router.post("/logout", authenticateToken, (req, res) =>
