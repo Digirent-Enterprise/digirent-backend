56713e0f0eb7d5ed6c47b00d9ae36658940d5e34
Giang2705 <s3914108@rmit.edu.vn>
2022-09-07 10:53:58 +0700

    fix update

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -3,45 +3,54 @@ const { uploadMultipleFiles } = require("../services/cloudinary.service");
 const { CloudinaryService, TransactionService } = require("../services");
 
 const productController = {
-  //add product
+    //add product
   addProduct: async (req, res) => {
     console.log(req.body);
     try {
-      const newProduct = new Product({
-        name: req.body.name,
-        brand: req.body.brand,
-        description: req.body.description,
-        images: req.body.images || [],
-        rentalCost: req.body.rentalCost,
-        rentalCostType: req.body.rentalCostType,
-        status: req.body.status || true,
-        serial: req.body.serial,
-        category: req.body.category,
-      });
-      const savedProduct = await newProduct.save();
-      await Category.findOneAndUpdate(
-        { name: req.body.category },
-        { $push: { products: savedProduct._id } },
-      ).catch((e) => console.log(e));
-      res.status(200).json(savedProduct);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+            const newProduct = new Product({
+                name: req.body.name,
+                category: req.body.category,
+                brand: req.body.brand,
+                description: req.body.description,
+                images: req.body.images || [],
+                rentalCost: req.body.rentalCost,
+                rentalCostType: req.body.rentalCostType,
+                status: req.body.status || true,
+                serial: req.body.serial,
+            });
+            if(files){
+                req.files.forEach(function(file, index, arr){
+                    let path = file.path + ','
+                    path = path.substring(0, path.lastIndexOf(","))
+                    newProduct.images.push(path)
+                })
+            }
 
-  // get all products
-  getAllProducts: async (req, res) => {
-    try {
-      const products = await Product.find();
-      res.status(200).json(products);
-    } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+            const savedProduct = await newProduct.save();
+            if(req.body.category){
+                const category = Category.findById(req.body.category);
+                await category.updateOne({$push: {products: savedProduct._id}});
+            }
+            res.status(200).json(savedProduct);
 
-  //get product
-  getProduct: async (req, res) => {
-    try {
+        } catch(err){
+            res.status(500).json(err);
+        }
+    },
+
+    // get all products
+    getAllProducts: async(req, res) => {
+        try {
+            const products = await Product.find();
+            res.status(200).json(products);
+        } catch(err) {
+            res.status(500).json(err);
+        }
+    },
+
+    //get product
+    getProduct: async(req, res) => {
+        try {
       const product = await Product.findById(req.params.id);
       const excludeIntervals =
         await TransactionService.getTransactionExcludeIntervals(req.params.id);
@@ -61,9 +70,9 @@ const productController = {
       };
       res.status(200).json(newMappingProduct);
     } catch (err) {
-      res.status(500).json(err);
-    }
-  },
+            res.status(500).json(err);
+        }
+    },
 
     // update product
     updateProduct: async(req, res) => {
@@ -75,7 +84,7 @@ const productController = {
                 await Category.updateMany({products: product._id}, {$pull: {products: product._id}});
 
                 // update into new category
-                const category = Category.findById(req.body.category);
+                const category = Category.find({name: req.body.category});
                 await category.updateOne({$push: {products: product._id}});
             }
 
@@ -86,18 +95,15 @@ const productController = {
         }
     },
 
-  // delete product
-  deleteProduct: async (req, res) => {
-    try {
-      await Category.updateMany(
-        { products: req.params.id },
-        { $pull: { products: req.params.id } },
-      );
-      await Product.findByIdAndDelete(req.params.id);
-      res.status(200).json("Delete successfully!");
-    } catch (err) {
-      res.status(500).json(err);
-    }
+    // delete product
+    deleteProduct: async(req, res) => {
+        try{
+            await Category.updateMany({products: req.params.id}, {$pull: {products: req.params.id}});
+            await Product.findByIdAndDelete(req.params.id);
+            res.status(200).json('Delete successfully!');
+        } catch(err){
+            res.status(500).json(err);
+        }
   },
 
   uploadSingleImage: async (req, res) => {
